2335865b1be2876052910efad065c8d7bb9779be
TrifShterev <tshterev@yahoo.com>
2020-09-07 10:33:52 +0300

    Add project files.

---
+++ Sum Matrix Elements/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace Sum_Matrix_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = ParseArrayFromConsole(',', ' ');
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+            int[,] matrix = FillingMatrixOfIntegers(rows, cols); long sum = 0;
+            foreach (var element in matrix)
+            {
+                sum += element;
+            }
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(sum);
+
+
+        }
+
+        private static int[,] FillingMatrixOfIntegers(int rows, int cols)
+        {
+            int[,] matrix = new int[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                int[] currentRow = Console.ReadLine()
+                .Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            return matrix;
+        }
+
+        static int [] ParseArrayFromConsole(params char[] splitSymbols)
+        {
+            return Console.ReadLine()
+                .Split(splitSymbols, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+        }
+    }
+}
