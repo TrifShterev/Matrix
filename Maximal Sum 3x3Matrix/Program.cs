ee0fa8bf2c430171ba5557d7eb2eb8365f2e0d0a
TrifShterev <tshterev@yahoo.com>
2020-09-22 12:15:29 +0300

    more tasks

---
+++ Maximal Sum 3x3Matrix/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+
+namespace Maximal_Sum_3x3Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+            var matrix = CreateMatrix(rows, cols);
+
+            int maxSum = int.MinValue;
+            int startCoordinateOfInnerMatrixRow = 0;
+            int startCoordinateOfInnerMatrixCol = 0;
+
+            for (int row  = 0; row < matrix.GetLength(0)-2; row++)
+            {
+                
+                for (int col = 0; col < matrix.GetLength(1)-2; col++)
+                {
+                    int sum = 0;
+
+                    sum += matrix[row, col] + matrix[row + 1, col] + matrix[row + 2, col];
+                    sum += matrix[row, col+1] + matrix[row , col+2] + matrix[row + 1, col+2];
+                    sum += matrix[row+2, col+1] + matrix[row+2 , col+2] + matrix[row + 1, col+1];
+                    
+                    
+                    if (maxSum < sum)
+                    {
+                        maxSum=sum;
+
+                        startCoordinateOfInnerMatrixRow = row;
+                        startCoordinateOfInnerMatrixCol = col;
+                    }
+                }
+               
+            }
+            Console.WriteLine($"Sum = {maxSum}");
+            for (int i = startCoordinateOfInnerMatrixRow; i < startCoordinateOfInnerMatrixRow+3; i++)
+            {
+                for (int j = startCoordinateOfInnerMatrixCol; j < startCoordinateOfInnerMatrixCol+3; j++)
+                {
+                    Console.Write(matrix[i,j] +" ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+        
+        private static int[,] CreateMatrix(int rows, int cols)
+        {
+            int[,] matrix = new int[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                
+                int[] currentRow = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            return matrix;
+        }
+    }
+    
+}
--- Radioactive Mutant Vampire Bunnies/Program.cs
+++ Radioactive Mutant Vampire Bunnies/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Radioactive_Mutant_Vampire_Bunnies
 {
@@ -6,7 +8,149 @@ namespace Radioactive_Mutant_Vampire_Bunnies
     {
         static void Main(string[] args)
         {
-            
+            int[] dimensions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+            char[,] matrix = new char[rows, cols];
+            int playerRow = -1;
+            int playerCol = -1;
+
+            for (int row = 0; row < rows; row++)
+            {
+                char[] colValues = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = colValues[col];
+                    if (colValues[col] == 'P')
+                    {
+                        playerRow = row;
+                        playerCol = col;
+                    }
+                }
+            }
+            char[] directions = Console.ReadLine().ToCharArray();
+            bool haveWon = false;
+
+            foreach (var direction in directions)
+            {
+                int currentPlayerRow = playerRow;
+                int currentPlayerCol = playerCol;
+                switch (direction)
+                {
+                    case 'U':
+                        currentPlayerRow--;
+                        break;
+                    case 'D':
+                        currentPlayerRow++;
+                        break;
+                    case 'L':
+                        currentPlayerCol--;
+                        break;
+                    case 'R':
+                        currentPlayerCol++;
+                        break;
+                }
+
+                haveWon = IsWon(matrix, currentPlayerRow, currentPlayerCol);
+
+                if (haveWon)
+                {
+                    matrix[playerRow, playerCol] = '.';
+                }
+                else
+                {
+                    if (matrix[currentPlayerRow, currentPlayerRow] == 'B')
+                    {
+                        matrix[playerRow, playerCol] = '.';
+                        playerRow = currentPlayerRow;
+                        playerCol = currentPlayerCol;
+                    }
+                    else
+                    {
+                        matrix[playerRow, playerCol] = '.';
+                        matrix[currentPlayerRow, currentPlayerCol] = 'P';
+                        playerRow = currentPlayerRow;
+                        playerCol = currentPlayerCol;
+                    }
+                }
+                List<int> bunnies = new List<int>();
+
+                for (int row = 0; row < rows; row++)
+                {
+                    for (int col = 0; col < cols; col++)
+                    {
+                        if (matrix[row, col] == 'B')
+                        {
+                            bunnies.Add(row);
+                            bunnies.Add(col);
+                        }
+                    }
+
+                }
+                for (int i = 0; i < bunnies.Count; i += 2)
+                {
+                    int bunnyRow = bunnies[i];
+                    int bunnyCol = bunnies[i + 1];
+
+                    SpreadBunny(matrix, bunnyRow, bunnyCol);
+                }
+                if (haveWon || matrix[playerRow, playerCol] == 'B')
+                {
+                    break;
+                }
+            }
+            PrintMatrix(matrix);
+            if (haveWon)
+            {
+                Console.WriteLine($"won: {playerRow} {playerCol}");
+            }
+            else
+            {
+                Console.WriteLine($"dead: {playerRow} {playerCol}");
+            }
+
+
+
+
+        }
+
+        private static void SpreadBunny(char[,] matrix, int bunnyRow, int bunnyCol)
+        {
+            if (bunnyRow - 1 >= 0)
+            {
+                matrix[bunnyRow - 1, bunnyCol] = 'B';
+            }
+            if (bunnyRow + 1 < matrix.GetLength(0))
+            {
+                matrix[bunnyRow + 1, bunnyCol] = 'B';
+            }
+            if (bunnyCol - 1 >= 0)
+            {
+                matrix[bunnyRow, bunnyCol - 1] = 'B';
+            }
+            if (bunnyCol + 1 < matrix.GetLength(1))
+            {
+                matrix[bunnyRow, bunnyCol + 1] = 'B';
+            }
+        }
+
+        private static bool IsWon(char[,] matrix, int currentPlayerRow, int currentPlayerCol)
+        {
+            int n = matrix.GetLength(0);
+            int m = matrix.GetLength(1);
+            return currentPlayerRow < 0 || currentPlayerRow >= n || currentPlayerCol < 0 || currentPlayerCol >= m;
+        }
+        private static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
